069ab680f69e2102a6db0ce835d7631bcd2b625a
Ahmed <ahmedmeabbas@gmail.com>
2022-07-13 23:13:41 +0200

    added inbox fragment layout

--- app/src/main/java/gmail/ahmedmeabbas/realestateapp/inbox/presentation/InboxFragment.kt
+++ app/src/main/java/gmail/ahmedmeabbas/realestateapp/inbox/presentation/InboxFragment.kt
@@ -4,8 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+import gmail.ahmedmeabbas.realestateapp.R
 import gmail.ahmedmeabbas.realestateapp.databinding.FragmentInboxBinding
+
+private const val NUM_TABS = 2
 
 class InboxFragment: Fragment() {
 
@@ -21,8 +28,70 @@ class InboxFragment: Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setUpViewPager()
+        setUpTabLayout()
+        setUpTabLayoutMediator()
+    }
+
+    private fun setUpViewPager() {
+        val pagerAdapter = InboxFragmentPagerAdapter(this)
+        binding.inboxViewPager.adapter = pagerAdapter
+    }
+
+    private fun setUpTabLayoutMediator() {
+        TabLayoutMediator(binding.inboxTabLayout, binding.inboxViewPager) { tab, position ->
+            if (position == 0) {
+                tab.text = getString(R.string.chats_tab)
+                tab.icon = ResourcesCompat.getDrawable(
+                    requireActivity().resources,
+                    R.drawable.custom_chat,
+                    requireActivity().theme
+                )
+            } else {
+                tab.text = getString(R.string.notifications_tab)
+                tab.icon = ResourcesCompat.getDrawable(
+                    requireActivity().resources,
+                    R.drawable.custom_notification,
+                    requireActivity().theme
+                )
+            }
+        }.attach()
+    }
+
+    private fun setUpTabLayout() {
+        binding.inboxTabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                binding.inboxViewPager.currentItem = tab!!.position
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+
+            }
+        })
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private inner class InboxFragmentPagerAdapter(fragment: Fragment) :
+        FragmentStateAdapter(fragment) {
+        override fun getItemCount() = NUM_TABS
+
+        override fun createFragment(position: Int): Fragment {
+            return when (position) {
+                0 -> ChatsFragment()
+                1 -> NotificationsFragment()
+                else -> ChatsFragment()
+            }
+        }
     }
 }
---
+++ app/src/main/java/gmail/ahmedmeabbas/realestateapp/inbox/presentation/NotificationsFragment.kt
@@ -0,0 +1,28 @@
+package gmail.ahmedmeabbas.realestateapp.inbox.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import gmail.ahmedmeabbas.realestateapp.databinding.FragmentNotificationsBinding
+
+class NotificationsFragment: Fragment() {
+
+    private var _binding: FragmentNotificationsBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentNotificationsBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
